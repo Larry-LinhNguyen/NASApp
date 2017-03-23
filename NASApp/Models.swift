857bd0c45b732eddec1ef05e67282b402980a7cd
Andrea Miotto <andreamiotto@MacBook-Pro-Andrea-Miotto.local>
2017-03-23 13:03:20 +0100

    adding networking code

---
+++ NASApp/Models.swift
@@ -0,0 +1,23 @@
+//
+//  Models.swift
+//  NASApp
+//
+//  Created by Andrea Miotto on 23/03/17.
+//  Copyright © 2017 Andrea Miotto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Daily {
+    let image: UIImage
+    let title: String
+    let description: String
+    
+    init(image: UIImage, title: String, description: String) {
+        self.image = image
+        self.title = title
+        self.description = description
+    }
+}
