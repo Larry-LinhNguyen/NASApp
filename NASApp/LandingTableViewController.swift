1f33a7aad9e56aafaa7de9ba9b46cac6b57646a1
Andrea Miotto <andreamiotto@MacBook-Pro-Andrea-Miotto.local>
2017-03-29 12:23:52 +0200

    pretty commented

--- NASApp/LandingTableViewController.swift
+++ NASApp/LandingTableViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 import AlamofireImage
 import SwiftyJSON
 
+
+/** This is the class for the **Landing Page**, the entry point */
 class LandingTableViewController: UITableViewController {
     
+    //----------------------
+    // MARK: - Variables
+    //----------------------
+    
+    ///It stores the data of the daily imagege
     var daily: Daily?
+    ///It stores all the data for the asteroids
     var asteroids: [Asteroid] = []
     
+    //----------------------
+    // MARK: - View funcs
+    //----------------------
     
     override func viewWillAppear(_ animated: Bool) {
+        //Start to download the data even beofre the view appears
         fetchingDaily()
         fetchingAsteroids()
     }
@@ -31,23 +43,75 @@ class LandingTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    //----------------------
+    // MARK: - Methods
+    //----------------------
+    
+    /** This func will download the daily data from the API */
+    func fetchingDaily() {
+        NetworkManager.fetchDaily {json in
+            do {
+                try self.daily = Daily(json: json)
+            } catch  let error {
+                self.displayAlert(title: "Error", message: "\(error)")
+            }
+        }
+    }
+    
+    /** This func will download the asteroids data from the API */
+    func fetchingAsteroids() {
+        NetworkManager.fetchAsteroids { json in
+            let objects = json["near_earth_objects"].dictionaryValue
+            for objectsByDate in objects {
+                let flatted = objectsByDate.value.flatMap { $0 }
+                for object in flatted {
+                    do {
+                        try self.asteroids.append(Asteroid(json: object.1))
+                    } catch let error {
+                        self.displayAlert(title: "Error", message: "\(error)")
+                    }
+                }
+            }
+        }
+    }
+    
+    //----------------------
+    // MARK: - Helpers
+    //----------------------
+    
+    /** This func will display the proper alert message for unaviable data */
+    func showDataUnaviableAlert() {
+        displayAlert(title: "Fetching Data", message: "We are moving the satellites for you, retry in a moment!")
+    }
+    
+    /**This func will display an Alert */
+    func displayAlert(title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    //----------------------
     // MARK: - Table view data source
-
+    //----------------------
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 4
     }
-
-   
-
     
+    //----------------------
     // MARK: - Navigation
+    //----------------------
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDaily" {
@@ -65,6 +129,7 @@ class LandingTableViewController: UITableViewController {
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         if identifier == "showDaily" {
+            //if the daily is nil, prompt an alert
             if (self.daily?.image == nil || self.daily?.title == nil || self.daily?.explanation == nil) {
                 showDataUnaviableAlert()
                 return false
@@ -73,6 +138,7 @@ class LandingTableViewController: UITableViewController {
         }
         
         if identifier == "showCollisions" {
+            //if the asteroids aren't downloaded, prompt an alert
             if (self.asteroids.count < 1) {
                 showDataUnaviableAlert()
                 return false
@@ -82,61 +148,6 @@ class LandingTableViewController: UITableViewController {
         
         // by default, transition
         return true
-    }
-    
-    func fetchingDaily() {
-        NetworkManager.fetchDaily {json in
-            //print(json)
-            
-            do {
-                try self.daily = Daily(json: json)
-            } catch  let error {
-                self.displayAlert(title: "Error", message: "\(error)")
-            }
-            
-        }
-    }
-    
-    func fetchingAsteroids() {
-        NetworkManager.fetchAsteroids { json in
-            let objects = json["near_earth_objects"].dictionaryValue
-            for objectsByDate in objects {
-                let flatted = objectsByDate.value.flatMap { $0 }
-                for object in flatted {
-                    do {
-                        try self.asteroids.append(Asteroid(json: object.1))
-                    } catch let error {
-                        self.displayAlert(title: "Error", message: "\(error)")
-                    }
-                }
-            }
-        }
-    }
-    
-    
-    func getDate() -> String {
-        let date = Date()
-        //creating the formatter and choosing the styles
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-            
-        //updating the title
-        return formatter.string(from: date)
-    }
-    
-    /**This func will display an Alert */
-    func displayAlert(title: String, message: String) {
-        
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(action)
-        
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    func showDataUnaviableAlert() {
-        displayAlert(title: "Fetching Data", message: "We are moving the satellites for you, retry in a moment!")
     }
 
 }
